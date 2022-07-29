eb1cde8c7c20c28f9bd8f43e51409bebc0e9c34e
iem5 <39573641+iem5@users.noreply.github.com>
2022-07-29 10:41:15 -0400

    Update data_standardization_helpers.py
    Added helper function for getting bias/fact ratings for the non-bias news-category orgs.

--- data_standardization_helpers.py
+++ data_standardization_helpers.py
@@ -32,11 +32,56 @@ def get_bias_fact_cred(report_list, bias):
         if(term in recognized_terms):
             term_type = recognized_terms[term]
             if(term_type in terms_output):
-                assert(terms_output[term_type] == term)
+                #if run into instance where have parsed out multiple biases, keep the more specific/extreme bias
+                if(terms_output[term_type] == 'LEFT' and term == 'FAR LEFT'):
+                    terms_output[term_type] = term
+                elif(terms_output[term_type] == 'RIGHT' and term == 'FAR RIGHT'):
+                    terms_output[term_type] = term
+                elif(terms_output[term_type] == 'FAR RIGHT' and term == 'RIGHT' or terms_output[term_type] == 'FAR LEFT' and term == 'LEFT'):
+                    pass
+                else:
+                    assert(terms_output[term_type] == term)
             else:
                 terms_output[term_type] = term
     if('Bias' not in terms_output):
         terms_output['Bias'] = bias
+    return terms_output
+
+
+def get_bias_fact_cred_conspiracy_pseudo(report_list):
+    recognized_terms = {'CENTER': 'Bias', 'FAR LEFT': 'Bias', 'LEFT': 'Bias', 'LEFT-CENTER': 'Bias', 'RIGHT-CENTER': 'Bias', 'RIGHT': 'Bias', 'FAR RIGHT': 'Bias',
+                    'VERY HIGH': 'Factual', 'HIGH': 'Factual', 'MOSTLY FACTUAL': 'Factual', 'MIXED': 'Factual', 'LOW': 'Factual', 'VERY LOW': 'Factual',
+                    'HIGH CREDIBILITY': 'Credibility', 'MEDIUM CREDIBILITY': 'Credibility', 'LOW CREDIBILITY': 'Credibility'}
+    mapping_terms = {'LEAST BIASED': 'CENTER', 'FAR-LEFT': 'FAR LEFT', 'LEFT BIASED': 'LEFT', 'RIGHT CONSPIRACY/PSEUDOSCIENCE': 'RIGHT',  'RIGHT-PSUEDOSCIENCE': 'RIGHT',
+                    'RIGHT CONSPIRACY- PSEUDOSCIENCE': 'RIGHT', 'EXTREME RIGHT CONSPIRACY': 'FAR RIGHT', 'RIGHT-CONSPIRACY-PSEUDOSCIENCE': 'RIGHT', 'RIGHT CONSPIRACY': 'RIGHT',
+                    'LEFT – PSEUDOSCIENCE': 'LEFT', 'EXTREME RIGHT': 'FAR RIGHT', 'RIGHT-CONSPIRACY': 'RIGHT', 'FAR-RIGHT CONSPIRACY-PSEUDOSCIENCE': 'FAR RIGHT', 
+                    'RIGHT PSEUDOSCIENCE': 'RIGHT', 'LEFT CONSPIRACY-PSEUDOSCIENCE': 'LEFT', 'LEFT-CONSPIRACY/PSEUDOSCIENCE': 'LEFT', 'RIGHT-CONSPIRACY/PSEUDOSCIENCE': 'RIGHT',
+                    'LEFT-PSEUDOSCIENCE': 'LEFT', 'VERY-LOW': 'VERY LOW', 'RIGHT-PSEUDOSCIENCE': 'RIGHT', 'RIGHT – CONSPIRACY': 'RIGHT', 'RIGHT CONSPIRACY – PSEUDOSCIENCE': 'RIGHT',
+                    'FAR RIGHT CONSPIRACY-PSEUSDOSCIENCE': 'FAR RIGHT', 'PSEUDOSCIENCE RIGHT BIASED': 'RIGHT', 'LEFT PSEUDOSCIENCE': 'LEFT', 'RIGHT – CONSPIRACY AND PSEUDOSCIENCE': 'RIGHT',
+                    'RIGHT CONSPIRACY-PSEUDOSCIENCE': 'RIGHT'}
+    terms_output = {}
+    for term in report_list:
+        term = term.strip()
+        if(term in mapping_terms):
+            term = mapping_terms[term]
+        if(term in recognized_terms):
+            term_type = recognized_terms[term]
+            if(term_type in terms_output):
+                #if run into instance where have parsed out multiple biases\fact ratings, keep the more specific/extreme bias
+                if(terms_output[term_type] == 'LEFT' and term == 'FAR LEFT'):
+                    terms_output[term_type] = term
+                elif(terms_output[term_type] == 'RIGHT' and term == 'FAR RIGHT'):
+                    terms_output[term_type] = term
+                elif(terms_output[term_type] == 'LOW' and term == 'VERY RIGHT'):
+                    terms_output[term_type] = term
+                elif(terms_output[term_type] == 'FAR RIGHT' and term == 'RIGHT' or terms_output[term_type] == 'FAR LEFT' and term == 'LEFT'):
+                    pass
+                elif(terms_output[term_type] == 'VERY LOW' and term == 'LOW'):
+                    pass
+                else:
+                    assert(terms_output[term_type] == term)
+            else:
+                terms_output[term_type] = term
     return terms_output
 
 def add_to_error_list(news_title, error_str, error_list):
